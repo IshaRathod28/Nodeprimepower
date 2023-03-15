09ef2f789ec53ec8b153047a1e2bc94261f1ae24
IshaRathod28 <124258565+IshaRathod28@users.noreply.github.com>
2023-03-15 16:14:11 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Primeinput from './components/Primeinput.js';
+import Powerinput from './components/Powerinput';
+
+
+
+function App() {
+  return (
+    <>
+    <div >
+   <Primeinput/>
+   <br></br>
+   <Powerinput/>
+  
+   
+    </div>
+    </>
+  );
+}
+
+export default App;
